1b544e1159c2ae717d582a6fa8b2f1ec91734bb7
Joris de Gruyter <jorisdg@gmail.com>
2025-10-26 12:37:10 -0700

    Ensure floats are decompiled to text using invariant culture. Add culture to the test cases (en-US and nl-BE as contrast). #5

--- src/DEvahebLibTests/BasicTests.cs
+++ src/DEvahebLibTests/BasicTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.IO;
 using DEvahebLib;
 using DEvahebLib.Visitors;
@@ -19,42 +20,75 @@ namespace DEvahebLibTests
         }
 
         [TestMethod]
-        [DataRow(@"BasicTests\camera")]
-        [DataRow(@"BasicTests\declare")]
-        [DataRow(@"BasicTests\do")]
-        [DataRow(@"BasicTests\dowait")]
-        [DataRow(@"BasicTests\flush")]
-        [DataRow(@"BasicTests\free")]
-        [DataRow(@"BasicTests\get")]
-        [DataRow(@"BasicTests\kill")]
-        [DataRow(@"BasicTests\move")]
-        [DataRow(@"BasicTests\play")]
-        [DataRow(@"BasicTests\print")]
-        [DataRow(@"BasicTests\random")]
-        [DataRow(@"BasicTests\remove")]
-        [DataRow(@"BasicTests\rotate")]
-        [DataRow(@"BasicTests\run")]
-        [DataRow(@"BasicTests\set")]
-        [DataRow(@"BasicTests\signal")]
-        [DataRow(@"BasicTests\sound")]
-        [DataRow(@"BasicTests\tag")]
-        [DataRow(@"BasicTests\use")]
-        [DataRow(@"BasicTests\wait")]
-        [DataRow(@"BasicTests\waitsignal")]
-        public void TestFunctions(string filenameBase)
+        [DataRow(@"BasicTests\camera", "en-US")]
+        [DataRow(@"BasicTests\declare", "en-US")]
+        [DataRow(@"BasicTests\do", "en-US")]
+        [DataRow(@"BasicTests\dowait", "en-US")]
+        [DataRow(@"BasicTests\flush", "en-US")]
+        [DataRow(@"BasicTests\free", "en-US")]
+        [DataRow(@"BasicTests\get", "en-US")]
+        [DataRow(@"BasicTests\kill", "en-US")]
+        [DataRow(@"BasicTests\move", "en-US")]
+        [DataRow(@"BasicTests\play", "en-US")]
+        [DataRow(@"BasicTests\print", "en-US")]
+        [DataRow(@"BasicTests\random", "en-US")]
+        [DataRow(@"BasicTests\remove", "en-US")]
+        [DataRow(@"BasicTests\rotate", "en-US")]
+        [DataRow(@"BasicTests\run", "en-US")]
+        [DataRow(@"BasicTests\set", "en-US")]
+        [DataRow(@"BasicTests\signal", "en-US")]
+        [DataRow(@"BasicTests\sound", "en-US")]
+        [DataRow(@"BasicTests\tag", "en-US")]
+        [DataRow(@"BasicTests\use", "en-US")]
+        [DataRow(@"BasicTests\wait", "en-US")]
+        [DataRow(@"BasicTests\waitsignal", "en-US")]
+
+        [DataRow(@"BasicTests\camera", "nl-BE")]
+        [DataRow(@"BasicTests\declare", "nl-BE")]
+        [DataRow(@"BasicTests\do", "nl-BE")]
+        [DataRow(@"BasicTests\dowait", "nl-BE")]
+        [DataRow(@"BasicTests\flush", "nl-BE")]
+        [DataRow(@"BasicTests\free", "nl-BE")]
+        [DataRow(@"BasicTests\get", "nl-BE")]
+        [DataRow(@"BasicTests\kill", "nl-BE")]
+        [DataRow(@"BasicTests\move", "nl-BE")]
+        [DataRow(@"BasicTests\play", "nl-BE")]
+        [DataRow(@"BasicTests\print", "nl-BE")]
+        [DataRow(@"BasicTests\random", "nl-BE")]
+        [DataRow(@"BasicTests\remove", "nl-BE")]
+        [DataRow(@"BasicTests\rotate", "nl-BE")]
+        [DataRow(@"BasicTests\run", "nl-BE")]
+        [DataRow(@"BasicTests\set", "nl-BE")]
+        [DataRow(@"BasicTests\signal", "nl-BE")]
+        [DataRow(@"BasicTests\sound", "nl-BE")]
+        [DataRow(@"BasicTests\tag", "nl-BE")]
+        [DataRow(@"BasicTests\use", "nl-BE")]
+        [DataRow(@"BasicTests\wait", "nl-BE")]
+        [DataRow(@"BasicTests\waitsignal", "nl-BE")]
+        public void TestFunctions(string filenameBase, string cultureName)
         {
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(cultureName);
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(cultureName);
             Assert.AreEqual(string.Empty, Helper.GenerateSourceFromIBIAndCompareOriginal(filenameBase, variables: variables));
         }
 
 
         [TestMethod]
-        [DataRow(@"BasicTests\affect")]
-        [DataRow(@"BasicTests\else")]
-        [DataRow(@"BasicTests\if")]
-        [DataRow(@"BasicTests\loop")]
-        [DataRow(@"BasicTests\task")]
-        public void TestBlocks(string filenameBase)
+        [DataRow(@"BasicTests\affect", "en-US")]
+        [DataRow(@"BasicTests\else", "en-US")]
+        [DataRow(@"BasicTests\if", "en-US")]
+        [DataRow(@"BasicTests\loop", "en-US")]
+        [DataRow(@"BasicTests\task", "en-US")]
+
+        [DataRow(@"BasicTests\affect", "nl-BE")]
+        [DataRow(@"BasicTests\else", "nl-BE")]
+        [DataRow(@"BasicTests\if", "nl-BE")]
+        [DataRow(@"BasicTests\loop", "nl-BE")]
+        [DataRow(@"BasicTests\task", "nl-BE")]
+        public void TestBlocks(string filenameBase, string cultureName)
         {
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(cultureName);
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(cultureName);
             Assert.AreEqual(string.Empty, Helper.GenerateSourceFromIBIAndCompareOriginal(filenameBase, variables: variables));
         }
     }
