433d4db8fe3939c3256b5df96e2d30bf315fe471
Joris de Gruyter <jorisdg@gmail.com>
2026-03-06 22:55:23 -0800

    Add tests for warning 1 and warning 2

--- src/DEvahebLibTests/ValidateTests.cs
+++ src/DEvahebLibTests/ValidateTests.cs
@@ -10,8 +10,8 @@ namespace DEvahebLibTests
         [DataRow(@"IcarusParserTests\errors\err001.txt", DiagnosticLevel.Error, 1)]
         [DataRow(@"IcarusParserTests\errors\err002.txt", DiagnosticLevel.Error, 2)]
         [DataRow(@"IcarusParserTests\errors\err003.txt", DiagnosticLevel.Error, 3)]
-        //[DataRow(@"IcarusParserTests\errors\war001.txt", DiagnosticLevel.Warning, 1)]
-        //[DataRow(@"IcarusParserTests\errors\war002.txt", DiagnosticLevel.Warning, 2)]
+        [DataRow(@"IcarusParserTests\errors\war001.txt", DiagnosticLevel.Warning, 1)]
+        [DataRow(@"IcarusParserTests\errors\war002.txt", DiagnosticLevel.Warning, 2)]
         [DataRow(@"IcarusParserTests\errors\war003.txt", DiagnosticLevel.Warning, 3)]
         public void TestValidations(string filename, DiagnosticLevel diagnosticLevel, int diagnosticCode)
         {
